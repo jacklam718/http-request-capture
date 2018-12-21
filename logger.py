660914c782189982d3431e3ec9b8eeb8a9eddb3f
jacklam718 <jacklam718@gmail.com>
2018-12-22 00:51:01 +0800

    Code refactoring

--- logger.py
+++ logger.py
@@ -1,6 +1,7 @@
 # --*--coding: utf-8 --*--
 
-import os
+from os import fsync
+from time import strftime
 
 REQ_URLS_FILE = open('req-urls.txt', 'a')
 
@@ -8,9 +9,11 @@ REQ_URLS_FILE = open('req-urls.txt', 'a')
 WHIYE = '\033[0m'
 GREEN = '\033[32m'
 
-def log(title, message='', write=False):
-    if write:
-        REQ_URLS_FILE.write(''.join([message, '\n']))
-        REQ_URLS_FILE.flush()
-        os.fsync(REQ_URLS_FILE.fileno())
-    print(''.join([GREEN, title, WHIYE, message]))
+def log(message=''):
+    loggedTime = strftime('%a, %d %b %Y %H:%M:%S %z: ')
+    print(''.join([GREEN, loggedTime, WHIYE, message]))
+
+def writeLog(message):
+    REQ_URLS_FILE.write(''.join([message, '\n']))
+    REQ_URLS_FILE.flush()
+    fsync(REQ_URLS_FILE.fileno())
